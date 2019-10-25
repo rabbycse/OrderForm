de7a1b7d540bed85d426ca189e9edffed2290eab
Rabbi <rabbycse7@gmail.com>
2019-10-25 15:55:33 +0600

    Add home work

---
+++ OrderItem/OrderItem/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace OrderItem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textOrderNo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textCustomerName = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textItem = new System.Windows.Forms.TextBox();
+            this.textQuantity = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.orderItemGridView = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.orderItemGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Order No:";
+            // 
+            // textOrderNo
+            // 
+            this.textOrderNo.Location = new System.Drawing.Point(106, 37);
+            this.textOrderNo.Name = "textOrderNo";
+            this.textOrderNo.Size = new System.Drawing.Size(100, 20);
+            this.textOrderNo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Customer Name:";
+            // 
+            // textCustomerName
+            // 
+            this.textCustomerName.Location = new System.Drawing.Point(107, 71);
+            this.textCustomerName.Name = "textCustomerName";
+            this.textCustomerName.Size = new System.Drawing.Size(572, 20);
+            this.textCustomerName.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox3.Location = new System.Drawing.Point(524, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "Order No";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(630, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 22);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Find";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(483, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Date:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox4.Location = new System.Drawing.Point(524, 38);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(155, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "dd/mm/yyyy";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(139, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Item:";
+            // 
+            // textItem
+            // 
+            this.textItem.Location = new System.Drawing.Point(180, 145);
+            this.textItem.Name = "textItem";
+            this.textItem.Size = new System.Drawing.Size(208, 20);
+            this.textItem.TabIndex = 9;
+            // 
+            // textQuantity
+            // 
+            this.textQuantity.Location = new System.Drawing.Point(524, 145);
+            this.textQuantity.Name = "textQuantity";
+            this.textQuantity.Size = new System.Drawing.Size(100, 20);
+            this.textQuantity.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(469, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Quantity:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(630, 145);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(49, 23);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // orderItemGridView
+            // 
+            this.orderItemGridView.AllowUserToOrderColumns = true;
+            this.orderItemGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.orderItemGridView.Location = new System.Drawing.Point(142, 174);
+            this.orderItemGridView.Name = "orderItemGridView";
+            this.orderItemGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.orderItemGridView.Size = new System.Drawing.Size(537, 178);
+            this.orderItemGridView.TabIndex = 13;
+            this.orderItemGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.orderItemGridView_CellClick);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(442, 358);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(523, 358);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 15;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(604, 358);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 16;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(771, 446);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.orderItemGridView);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textQuantity);
+            this.Controls.Add(this.textItem);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textCustomerName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textOrderNo);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "OrderForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.orderItemGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textOrderNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textCustomerName;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textItem;
+        private System.Windows.Forms.TextBox textQuantity;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView orderItemGridView;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ OrderItem/OrderItem/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace OrderItem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=RABBI-LAPTOP;Initial Catalog=OrderDb;Integrated Security=True");
+        public int OrderNo;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetOrderItems();
+        }
+
+        private void GetOrderItems()
+        {
+            SqlCommand cmd = new SqlCommand("Select OrderNo as 'SL', Item, Quantity from OrderItems", con);
+            DataTable dt = new DataTable();
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+
+            orderItemGridView.DataSource = dt;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (isValid())
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO OrderItems VALUES (@Item, @Quantity)", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@Item", textItem.Text);
+                cmd.Parameters.AddWithValue("@Quantity", textQuantity.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                GetOrderItems();
+            }
+        }
+
+        private bool isValid()
+        {
+            if (textItem.Text == string.Empty)
+            {
+                MessageBox.Show("Order Item is required", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            orderItemGridView[1, orderItemGridView.Rows.Count - 1].Value = "Total Quantity";
+            orderItemGridView.Rows[orderItemGridView.Rows.Count - 1].Cells[2].Style.BackColor = Color.White;
+            orderItemGridView.Rows[orderItemGridView.Rows.Count - 1].Cells[2].Style.ForeColor = Color.Black;
+            
+            decimal total = 0;
+            for(int i = 0; i < orderItemGridView.RowCount - 1; i++)
+            {
+                var value = orderItemGridView.Rows[i].Cells[2].Value;
+                if(value != DBNull.Value)
+                {
+                    total += Convert.ToDecimal(value);
+                }
+            }
+            orderItemGridView.Rows[orderItemGridView.Rows.Count - 1].Cells[2].Value = total.ToString();
+            MessageBox.Show("New order is successfully saved in the database", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (OrderNo > 0)
+            {
+                SqlCommand cmd = new SqlCommand("update OrderItems set Item=@Item,Quantity=@Quantity where OrderNo=@No", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@Item", textItem.Text);
+                cmd.Parameters.AddWithValue("@Quantity", textQuantity.Text);
+                cmd.Parameters.AddWithValue("@No", OrderNo);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("OrderItem is successfully update in the database", "Updated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                GetOrderItems();
+            }
+            else
+            {
+                MessageBox.Show("Please select an item to update", "Select?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (OrderNo != 0)
+            {
+                SqlCommand cmd = new SqlCommand("delete OrderItems where OrderNo=@No", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@No", OrderNo);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Item Deleted Successfully!");
+                GetOrderItems();
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select Item to Delete");
+            }
+        }
+
+        private void orderItemGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            OrderNo = Convert.ToInt32(orderItemGridView.Rows[0].Cells[0].Value);
+            textItem.Text = orderItemGridView.SelectedRows[0].Cells[1].Value.ToString();
+            textQuantity.Text = orderItemGridView.SelectedRows[0].Cells[2].Value.ToString();
+        }
+    }
+}
